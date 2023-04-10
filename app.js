96bf116f03619b481c4f31a957dfc94e66b7c0be
Felix Yang <fzyang1227@gmail.com>
2023-04-09 20:21:50 -0400

    user env

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ import HelloController from "./controllers/hello-controller.js"
 import UserController from "./controllers/users/users-controller.js"
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose";
+const USER = process.env.MONGO_WEBDEV_USER;
 const PASS = process.env.MONGO_WEBDEV_PASS;
-const CONNECT_STRING=`mongodb+srv://webdev-user:${PASS}@cluster0.csrkvxi.mongodb.net/?retryWrites=true&w=majority`;
+const CONNECT_STRING=`mongodb+srv://${USER}:${PASS}@cluster0.csrkvxi.mongodb.net/?retryWrites=true&w=majority`
+|| 'mongodb://127.0.0.1:27017/tuiter';
 mongoose.connect(CONNECT_STRING);
 
 const app = express();
